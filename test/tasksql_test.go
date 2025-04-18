7d587eae1c4a74f690a1e582544f737c03eb2e12
Hector Steven Villasano <hectorstevenvillasano@gmail.com>
2025-04-17 22:39:39 -0400

    - rename db with testdb-uui - remove db after test is complete - Test_CreateTestData will loop over tables and create data with uuid - Test_Get will loop over tables to create data with uuid

--- test/tasksql_test.go
+++ test/tasksql_test.go
@@ -19,7 +19,8 @@ var cfg config
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err := tasksql.NewDB("test.db")
+	dbName := fmt.Sprintf("testdb_%s", uuid.NewString())
+	testDB, err := tasksql.NewDB(dbName)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,41 +28,42 @@ func TestMain(m *testing.M) {
 
 	cfg = config{
 		testDB:     testDB,
-		tableNames: []string{"users", "data"},
+		tableNames: []string{"users", "data", "notes"},
 	}
 
 	code := m.Run()
-	log.Println("[TEST START]tasksql")
+	// log.Println("[TEST START]tasksql")
 
+	os.Remove(dbName)
 	os.Exit(code)
-	log.Println("[TEST FINISHED]tasksql")
+	// log.Println("[TEST FINISHED]tasksql")
 }
 
 func Test_CreateTestData(t *testing.T) {
-	tableName := "data"
-	err := cfg.testDB.CreateTableIfNotExist(tableName)
-	if err != nil {
-		t.Fatalf("[Test_CreateTableIfNotExist] %s", err)
-	}
-
-	for range 4 {
-		text := fmt.Sprintf("text%v", uuid.NewString())
-		err = cfg.testDB.Post(tableName, text)
+	for _, table := range cfg.tableNames {
+		err := cfg.testDB.CreateTableIfNotExist(table)
 		if err != nil {
-			t.Fatal(err)
+			t.Fatalf("[Test_CreateTestData]: %s", err)
+		}
+
+		for range 12 {
+			text := fmt.Sprintf("text%v", uuid.NewString())
+			err = cfg.testDB.Post(table, text)
+			if err != nil {
+				t.Fatalf("[Test_CreateTestData]: %s", err)
+			}
 		}
 	}
-
 }
 
 func Test_Get(t *testing.T) {
-	data, err := cfg.testDB.Get("")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, text := range data {
-		log.Println(text)
+	// var data []string
+	// var err error
+	for _, table := range cfg.tableNames {
+		_, err := cfg.testDB.Get(table)
+		if err != nil {
+			t.Fatal(err)
+		}
 	}
 }
 
@@ -85,9 +87,8 @@ func Test_IsValidTableID(t *testing.T) {
 
 	for _, tc := range testCases {
 		actual := tasksql.IsValidTableID(tc.input)
-		log.Println("expected: false\nactual: ", tc.expected)
 		if actual != tc.expected {
-			t.Fatalf("expected: false\nactual: %v", actual)
+			t.Fatalf("expected: false actual: %v", tc.expected)
 		}
 	}
 }
